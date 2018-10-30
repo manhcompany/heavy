b2043139cb8430df511c9405b370e6f019b55ef6
manh <manh@trustingsocial.com>
2018-10-30 17:17:01 +0700

    add Show decorator for Operator

--- core/src/main/scala/com/heavy/core/utils/SparkOperator.scala
+++ core/src/main/scala/com/heavy/core/utils/SparkOperator.scala
@@ -2,16 +2,33 @@ package com.heavy.core.utils
 
 import org.apache.spark.sql.DataFrame
 
+abstract class OperatorDecorator(decoratedOperator: Operator[DataFrame]) extends Operator[DataFrame] {
+  override def execute(operands: DataFrame*): Option[DataFrame] = decoratedOperator.execute(operands: _*)
+}
+
+class ShowDataFrame(decoratedOperator: Operator[DataFrame]) extends OperatorDecorator(decoratedOperator) {
+  override def getNumberOperator: Int = decoratedOperator.getNumberOperator
+
+  override def execute(operands: DataFrame*): Option[DataFrame] = {
+    val result = decoratedOperator.execute(operands: _*)
+    result match {
+      case Some(df) => {
+        df.show()
+        result
+      }
+      case None => None
+    }
+  }
+}
+
 object SparkOperator {
   class InputOperator(config: OperatorConfig) extends Operand[DataFrame] {
     override def execute(operands: DataFrame*) : Option[DataFrame] = {
       val spark = SparkCommon.getSparkSession
-      val result = config.options.get.foldLeft(config.format match {
+      Some(config.options.get.foldLeft(config.format match {
         case Some(f) => spark.read.format(f)
         case None => spark.read
-      })((x, y) => x.option(y.key, y.value)).load(config.path.get)
-      result.show()
-      Some(result)
+      })((x, y) => x.option(y.key, y.value)).load(config.path.get))
     }
   }
 
@@ -48,18 +65,14 @@ object SparkOperator {
       dfL.createOrReplaceTempView("dfl")
       dfR.createOrReplaceTempView("dfr")
       val spark = SparkCommon.getSparkSession
-      val result = spark.sql(s"select ${config.select.get.head} from dfl ${config.joinType.getOrElse("inner")} join dfr on ${config.conditions.get}")
-      result.show
-      Some(result)
+      Some(spark.sql(s"select ${config.select.get.head} from dfl ${config.joinType.getOrElse("inner")} join dfr on ${config.conditions.get}"))
     }
   }
 
   class SelectOperator(config: OperatorConfig) extends UnaryOperator[DataFrame] {
     override def execute(operands: DataFrame*): Option[DataFrame] = {
       val df = operands.head
-      val result = df.selectExpr(config.select.get: _*)
-      result.show()
-      Some(result)
+      Some(df.selectExpr(config.select.get: _*))
     }
   }
 
@@ -69,49 +82,43 @@ object SparkOperator {
     }
 
     override def execute(operands: DataFrame*): Option[DataFrame] = {
-      val result = operands.tail.foldLeft(operands.head)((r, d) => r.union(d))
-      result.show()
-      Some(result)
+      Some(operands.tail.foldLeft(operands.head)((r, d) => r.union(d)))
     }
   }
 
   class DeduplicateOperator(config: OperatorConfig) extends UnaryOperator[DataFrame] {
     override def execute(operands: DataFrame*): Option[DataFrame] = {
-      val result = config.cols match {
+      Some(config.cols match {
         case Some(cs) => operands.head.dropDuplicates(cs)
         case None => operands.head.dropDuplicates()
-      }
-      result.show()
-      Some(result)
+      })
     }
   }
 
   class DropOperator(config: OperatorConfig) extends UnaryOperator[DataFrame] {
     override def execute(operands: DataFrame*): Option[DataFrame] = {
-      val result = operands.head.drop(config.cols.get: _*)
-      result.show()
-      Some(result)
+      Some(operands.head.drop(config.cols.get: _*))
     }
   }
 
   class RenameOperator(config: OperatorConfig) extends UnaryOperator[DataFrame] {
     override def execute(operands: DataFrame*): Option[DataFrame] = {
-      val result = config.renamed.get.foldLeft(operands.head)((df, col) => df.withColumnRenamed(col._1, col._2))
-      result.show()
-      Some(result)
+      Some(config.renamed.get.foldLeft(operands.head)((df, col) => df.withColumnRenamed(col._1, col._2)))
     }
   }
 
   def apply(config: OperatorConfig): Operator[DataFrame] = {
-    config.name match {
-      case "input" => new InputOperator(config = config)
-      case "output" => new OutputOperator(config=config)
-      case "select" => new SelectOperator(config=config)
-      case "join" => new JoinOperator(config=config)
-      case "union" => new UnionOperator(config=config)
-      case "dedup" => new DeduplicateOperator(config=config)
-      case "drop" => new DropOperator(config=config)
-      case "rename" => new RenameOperator(config=config)
-    }
+    new ShowDataFrame(
+      config.name match {
+        case "input" => new InputOperator(config = config)
+        case "output" => new OutputOperator(config=config)
+        case "select" => new SelectOperator(config=config)
+        case "join" => new JoinOperator(config=config)
+        case "union" => new UnionOperator(config=config)
+        case "dedup" => new DeduplicateOperator(config=config)
+        case "drop" => new DropOperator(config=config)
+        case "rename" => new RenameOperator(config=config)
+      }
+    )
   }
 }
